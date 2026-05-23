b2a7837c4a7f457101091c16d2a5802b8cd76346
Gabrielv1998 <gabrielv98@hotmail.com>
2026-05-23 09:39:17 -0300

    inicializar repo

---
+++ src/main/java/unlar/edu/ar/parcialprog3/Parcialprog3Application.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.parcialprog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcialprog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcialprog3Application.class, args);
+	}
+
+}
---
+++ src/test/java/unlar/edu/ar/parcialprog3/Parcialprog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.parcialprog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcialprog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
